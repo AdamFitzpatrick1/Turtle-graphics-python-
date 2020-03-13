3d357bc893f7b19a608158f1b54c3ed07a59379a
AdamFitzpatrick1 <31987448+AdamFitzpatrick1@users.noreply.github.com>
2020-03-13 13:29:08 +0000

    updated code

---
+++ portfolio 5 new as of 06.03.2020 modified.py
@@ -0,0 +1,184 @@
+import turtle
+import time 
+screen = turtle.Screen()
+t = turtle.Turtle()
+screen.tracer(0) 
+sizescreen = 1000
+screen.setup(sizescreen,sizescreen)
+t.penup()
+def drawA():
+
+    #Draws the A 
+       t.pendown() 
+       t.right(70)
+       t.forward(50)    
+       t.backward(50)
+       t.right(45)
+       t.forward(50)
+       t.backward(25)
+       t.right(245)
+       t.forward(30)
+       t.penup()
+       t.forward(5)
+  
+
+def drawF():
+
+    #Draws the F 
+        t.pendown()
+        t.left(90)
+        t.forward(50)
+        t.right(90)
+        t.forward(50) 
+        t.backward(50)
+        t.forward(50)
+        t.right(180)
+        t.forward(50)
+        t.right(270)
+        t.pendown()
+        t.forward(25)
+        t.right(-90)
+      
+        t.forward(30)
+        t.penup()
+
+#Draw stickman
+
+def stickman(xp, yp):
+
+        x = xp
+        y = yp
+        headSize = 25
+        ninetyDegree = 90
+        thirtypixels = 30
+        minusseventy = -70
+        minusonetwenty = -120
+        leftminustwothirty = -230
+        forwardfifthteen = 15
+        minusoneeightyright = -180
+        oneeightyleft = 180
+        forwardfifty = 50 
+        minusfiftyforward = -50
+        forwardeighty = 80
+        widthbetween = 70
+        #for i in range(0,5):
+
+
+        t.goto(x,y)
+        t.pendown();
+        t.circle(headSize)
+        t.right(ninetyDegree)
+        t.forward(thirtypixels)
+        t.left(minusseventy)
+        t.forward(thirtypixels)
+        t.backward(thirtypixels)
+        t.right(minusonetwenty)
+        t.forward(thirtypixels)
+        t.backward(thirtypixels)
+        t.left(leftminustwothirty)
+        t.penup();
+        t.forward(forwardfifthteen)
+        t.pendown()
+        t.penup()
+        t.right(ninetyDegree)
+        t.forward(thirtypixels)
+        t.right(minusoneeightyright)
+        t.forward(thirtypixels)
+        t.forward(thirtypixels)
+        t.left(oneeightyleft)
+        t.pendown()
+        t.forward(forwardfifty)
+                
+        t.forward(minusfiftyforward)
+        
+       
+        
+        t.penup()
+        #t.forward(thirtypixels)
+        #t.forward(forwardeighty)
+      
+                
+                
+        x = x + widthbetween
+        t.setx(x)
+        t.sety(y)
+        
+x = 90
+y = 90  
+
+#Draw stickman but keep them static (not animated)
+def stickmanstatic():
+    x = -60
+    y = -60
+    for i in range(0,6):
+
+
+        t.goto(x,y)
+        t.pendown();
+        t.circle(25) #head 
+        t.right(90)
+        t.forward(30)
+        t.left(-70)
+        t.forward(30)
+        t.backward(30)
+        t.right(-120)
+        t.forward(30)
+        t.backward(30)
+        t.left(-230)
+        t.penup();
+        t.forward(15)
+        t.pendown()
+        t.penup()
+        t.right(90)
+        t.forward(30)
+        t.right(-180)
+        t.forward(30)
+        t.forward(30)
+        t.left(180)
+        t.pendown()
+        t.forward(30)
+        
+        t.forward(-30)
+    
+        t.pendown()
+      
+        
+        t.penup()
+        t.forward(30)
+        t.forward(87)
+        t.pendown()
+        
+        
+        x = x + 70
+        
+        t.setx(x)
+        t.sety(y)
+       
+
+
+while True:
+
+        t.clear()
+        #Draw stuff in here
+        drawA()
+        drawF()
+        stickman(x, y)
+        stickmanstatic()
+        time.sleep(0.1)
+        screen.update()
+        #Do maths here 
+        x =  x + 20 
+        y =  y + 20
+        x = x - 20
+        y = y + -10
+       
+
+      
+        
+        
+       
+
+
+
+
+
